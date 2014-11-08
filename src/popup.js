5f2df9c76029ecb92b8ec2c7e000b8eb1a85a911
moonkin63 <moonkin63>
2014-11-09 01:21:38 +0400

    added icons remove button from popup

--- src/popup.js
+++ src/popup.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function () {
-    $('#md_button_start').click(function () {
-        $('#test_text').html($('#test_text').html() + '<br>+1');
-        chrome.tabs.getSelected(null, function (tab) {
-            chrome.tabs.sendMessage(tab.id, {action: "start_timer"}, function (response) {
-            });
-        });
-    });
+//    $('#md_button_start').click(function () {
+//        $('#test_text').html($('#test_text').html() + '<br>+1');
+//        chrome.tabs.getSelected(null, function (tab) {
+//            chrome.tabs.sendMessage(tab.id, {action: "start_timer"}, function (response) {
+//            });
+//        });
+//    });
 });
